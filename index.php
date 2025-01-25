3c38356da2f8b2dc746dee57a70028e5799bb662
sujaya-mindev <sujaya.manith23@gmail.com>
2025-01-25 14:44:34 +0530

    modifications to index.php

--- index.php
+++ index.php
@@ -14,15 +14,96 @@ $result = $conn->query($query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Shop</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
     <?php include 'navbar.html'; ?>
 
-    <!-- Hero Section -->
-    <div class="bg-primary text-white text-center py-5">
-        <h1>Welcome to Our Online Shop</h1>
-        <p>Discover amazing products at great prices!</p>
+    <!-- Carousel Section -->
+    <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+            <img src="images/index/banner/banner-1.avif" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="images/index/banner/banner-2.avif" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="images/index/banner/banner-3.avif" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="images/index/banner/banner-4.avif" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="images/index/banner/banner-5.avif" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
     </div>
+
+    <!-- Promotions -->
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <img src="images/index/promo/promotion_section-1.avif" id="promo" alt="Promotion 1">
+            </div>
+            <div class="col">
+                <img src="images/index/promo/promotion_section-2.avif" id="promo" alt="Promotion 2">
+            </div>
+            <div class="col">
+                <img src="images/index/promo/promotion_section-3.avif" id="promo" alt="Promotion 3">
+            </div>
+        </div>
+    </div>
+
+    <!-- Featured Products -->
+    <div id="bestsellers">
+        <h2>Best Sellers</h2>
+    </div>
+    <div id="productCarousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <?php 
+            $rowCount = 0;
+            $itemsPerRow = 6;
+            $activeClass = "active";
+            while ($row = $result->fetch_assoc()): 
+                if ($rowCount % $itemsPerRow === 0): ?>
+                    <div class="carousel-item <?php echo $activeClass; ?>">
+                        <div class="row">
+                <?php 
+                $activeClass = "";
+                endif; ?>
+
+                <div class="col-md-4 col-lg-3 mb-4" id="col-lg-3">
+                    <div id="bestsellers">
+                        <img src="<?php echo $row['product_image']; ?>" class="card-img-top" alt="<?php echo $row['product_name']; ?>">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $row['product_name']; ?></h5>
+                            <p class="card-text">$<?php echo number_format($row['product_price'], 2); ?></p>
+                        </div>
+                    </div>
+                </div>
+            
+                <?php 
+                $rowCount++;
+                if ($rowCount % $itemsPerRow === 0 || $rowCount === $result->num_rows):?>
+                        </div>
+                    </div>
+                <?php endif; 
+            endwhile; ?>
+        </div>
+    </div>
+
+
+
+
 
     <!-- Product Grid -->
     <div class="container my-5">
