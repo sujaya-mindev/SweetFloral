b22bcbcf4c38f92dbbe83b9f41b8b821392ad616
sujaya-mindev <sujaya.manith23@gmail.com>
2025-01-26 10:52:27 +0530

    db update

--- secretcore.sql
+++ secretcore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 24, 2025 at 09:39 AM
+-- Generation Time: Jan 26, 2025 at 06:20 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -42,6 +42,8 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_code`, `product_name`, `product_description`, `product_image`, `product_category`, `product_stock`, `product_price`) VALUES
+('CAKESLCOM4', 'FLOWERY GARDEN RIBBON CAKE 1KG (2.2LBS)', 'Artfully decorated ribbon cake suitable for any occasion made with our signature ribbon cake recipe, this cake is a truly delightful gift to any recipient.\r\n', 'images\\cakes\\CAKESLCOM4.avif', 'cakes', 100, 5250),
+('CAKESLCOM6', 'PASTEL TEAL RIBBON CAKE 750G (1.6LBS)', 'This pretty little pastel teal shade cake is a delicious ribbon cake, covered with fluffy buttercream and sprinkled with colored funfetties to make it extra special.', 'images\\cakes\\CAKESLCOM6.avif', 'cakes', 100, 4200),
 ('FLOWERLCOM21', 'PRETTIEST EVER', 'This beautiful flower bunch features lovely red lilies, a striking expression of nature\'s elegance. Their vibrant petals create an enchanting display of grace and charm. A heartfelt gift that exudes simplicity and timeless beauty, perfect for adding warmth and sophistication to any celebration.​​​​​​​', 'images\\flowers\\FLOWERLCOM21.avif', 'flowers', 100, 2900),
 ('FLOWERLCOM351', 'FLORIA', 'Elevate your Avurudu celebrations with our stunning flower bunche, symbolizing renewal and prosperity. Crafted with care, these vibrant bunch bring joy to any home, embodying the spirit of the New Year. Welcome abundance and happiness as you adorn your surroundings with our exquisite floral creations.\r\n', 'images\\flowers\\FLOWERLCOM351.avif', 'flowers', 100, 4850),
 ('FLOWERLCOM465', 'BLOOMING KNOWLEDGE', 'A beautiful assortment of fresh blooms, thoughtfully arranged to create an elegant and eye-catching bouquet. Perfect for celebrating special moments or brightening up any space, this floral bunch is designed to spread joy and bring a smile to the recipient’s face.', 'images\\flowers\\FLOWERLCOM465.avif', 'flowers', 100, 1800),
