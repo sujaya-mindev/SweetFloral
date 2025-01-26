845e3f0cf5a319c4d3b33fa083736576f7dd2d61
sujaya-mindev <sujaya.manith23@gmail.com>
2025-01-26 20:19:56 +0530

    🫠

--- index.php
+++ index.php
@@ -13,7 +13,8 @@ $result = $conn->query($query);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Shop</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
@@ -52,77 +53,70 @@ $result = $conn->query($query);
     <div class="container">
         <div class="row">
             <div class="col">
-                <img src="images/index/promo/promotion_section-1.avif" id="promo" alt="Promotion 1">
+                <img src="images/index/promo/promotion_section-1.avif" id="promo" class="img-fluid" alt="Promotion 1">
             </div>
             <div class="col">
-                <img src="images/index/promo/promotion_section-2.avif" id="promo" alt="Promotion 2">
+                <img src="images/index/promo/promotion_section-2.avif" id="promo" class="img-fluid" alt="Promotion 2">
             </div>
             <div class="col">
-                <img src="images/index/promo/promotion_section-3.avif" id="promo" alt="Promotion 3">
+                <img src="images/index/promo/promotion_section-3.avif" id="promo" class="img-fluid" alt="Promotion 3">
             </div>
         </div>
     </div>
 
-    <!-- Featured Products -->
-    <div id="bestsellers">
-        <h2>Best Sellers</h2>
-    </div>
-    <div id="productCarousel" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-            <?php 
-            $rowCount = 0;
-            $itemsPerRow = 6;
-            $activeClass = "active";
-            while ($row = $result->fetch_assoc()): 
-                if ($rowCount % $itemsPerRow === 0): ?>
-                    <div class="carousel-item <?php echo $activeClass; ?>">
-                        <div class="row">
-                <?php 
-                $activeClass = "";
-                endif; ?>
-
-                <div class="col-md-4 col-lg-3 mb-4" id="col-lg-3">
-                    <div id="bestsellers">
-                        <img src="<?php echo $row['product_image']; ?>" class="card-img-top" alt="<?php echo $row['product_name']; ?>">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $row['product_name']; ?></h5>
-                            <p class="card-text">$<?php echo number_format($row['product_price'], 2); ?></p>
+    <!-- Categories -->
+    <div class="container-fluid" id="categories">
+        <div class="row">
+            <div class="card" id="category-card">
+                <div class="row">
+                    <div class="col" id="category-image">
+                        <img src="images/index/flowers.png" class="img-fluid" alt="..." style="padding-left: 5px; width: 90%; margin: 10px;">
+                    </div>
+                    <div class="col" id="category-image">
+                        <img src="images/index/1.png" class="img-fluid" alt="...">
+                        <div class="card-img-overlay">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small>Last updated 3 mins ago</small></p>
                         </div>
                     </div>
                 </div>
-            
-                <?php 
-                $rowCount++;
-                if ($rowCount % $itemsPerRow === 0 || $rowCount === $result->num_rows):?>
-                        </div>
-                    </div>
-                <?php endif; 
-            endwhile; ?>
+            </div>
+            <div class="card" id="category-card">
+                <img src="images/index/cakes.png" class="img-fluid" alt="...">
+                <div class="card-img-overlay">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                </div>
+            </div>
         </div>
     </div>
+        
+    
+    
+    <!-- Featured Products 
+    <div id="bestsellers">
+        <h2>Best Sellers</h2>
+    </div>
 
-
-
-
-
-    <!-- Product Grid -->
-    <div class="container my-5">
-        <div class="row">
+    <div class="container my-4" id="product-grid">
+        <div id="productRow" class="d-flex overflow-auto">
             <?php while ($row = $result->fetch_assoc()): ?>
-                <div class="col-md-4 col-lg-3 mb-4">
-                    <div class="card product-card">
-                        <img src="images/<?php echo $row['product_image']; ?>" class="card-img-top" alt="<?php echo $row['product_name']; ?>">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $row['product_name']; ?></h5>
-                            <p class="card-text"><?php echo $row['product_description']; ?></p>
-                            <p class="card-text">$<?php echo number_format($row['product_price'], 2); ?></p>
-                            
-                        </div>
+                <div class="card product-card" id="product-card">
+                    <img src="<?php echo $row['product_image']; ?>" class="img-fluid" alt="<?php echo $row['product_name']; ?>">
+                    <div class="card-body text-center">
+                        <h5 class="card-title" id="product-title"><?php echo $row['product_name']; ?></h5>
+                    </div>
+                    <div class="card-footer" id="product-price">
+                        <small class="text-body-secondary">Rs: <?php echo number_format($row['product_price'], 2); ?></small>
                     </div>
                 </div>
             <?php endwhile; ?>
         </div>
     </div>
+    
+    -->
 
     <?php include 'footer.html'; ?>
 
